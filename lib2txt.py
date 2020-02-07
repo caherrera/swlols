798e7f500dc285199367e5a95c99f57c628c0b0d
Carlos Herrera <ca.herrera@icloud.com>
2020-02-07 12:54:41 -0300

    lib2txt.py

---
+++ lib2txt.py
@@ -0,0 +1,29 @@
+import glob
+import json
+import sys
+
+
+def load_json(file):
+    with open(file, 'r') as myfile:
+        data = myfile.read()
+    obj = json.loads(data)
+    return obj
+
+
+if len(sys.argv) > 1:
+    output = sys.argv[1]
+
+    path = './lib/domains/'
+    files = [f for f in glob.glob(path + "**/*.json", recursive=True)]
+
+    hosts = []
+
+    for f in files:
+        j = load_json(f)
+        hosts.append((j['host']))
+
+    hosts.sort()
+
+    o = open(output, 'w')
+    o.write("\n".join(hosts))
+    o.close()
